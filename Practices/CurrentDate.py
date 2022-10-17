bf91bb55a00ba2f9ebeeb7afda68da5995b7cc9f
Sophie <65602967+Sophed@users.noreply.github.com>
2022-10-17 15:18:27 +0100

    Remove practices tasks, tbh i dont know why i added them

--- Practices/CurrentDate.py
+++
@@ -1,3 +0,0 @@
-from datetime import date
-today = date.today().strftime("%d/%m/%y")
-print(d1)
--- Practices/ReadWrite.py
+++
@@ -1,5 +0,0 @@
-import random
-
-with open("test.txt", "a") as f:
-    f.write(str(random.randint(100, 999)) + "\n")
-    f.close
